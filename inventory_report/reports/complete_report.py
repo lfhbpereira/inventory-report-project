a445a36ce0a6b52a0cac8d7575733b303e506b1b
Luiz Pereira <lfhbpereira@gmail.com>
2023-05-01 15:53:30 -0300

    feat(req03): add generate method in CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(list):
+        simple_report = SimpleReport.generate(list)
+
+        companies = [item["nome_da_empresa"] for item in list]
+        companies_products = Counter(companies).items()
+
+        companies_total = ""
+        for company, quantity in companies_products:
+            companies_total += f"- {company}: {quantity}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{companies_total}"
+        )
