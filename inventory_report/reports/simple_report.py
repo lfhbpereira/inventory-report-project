f9154e325711ae060279d59587b4f1401b90196c
Luiz Pereira <lfhbpereira@gmail.com>
2023-05-01 15:30:34 -0300

    feat(req02): add generate method in SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,26 @@
+from collections import Counter
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list):
+        oldest_manufacturing_date = min(
+            item["data_de_fabricacao"]
+            for item in list
+        )
+
+        nearest_expiration_date = min(
+            item["data_de_validade"]
+            for item in list
+            if item["data_de_validade"] > str(datetime.today())
+        )
+
+        companies = [item["nome_da_empresa"] for item in list]
+        company_with_most_products = Counter(companies).most_common()[0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {nearest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_most_products[0]}"
+        )
