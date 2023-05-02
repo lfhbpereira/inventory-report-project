b1422c493d4bd0eecac6b5426f94f2642a23d6eb
Luiz Pereira <lfhbpereira@gmail.com>
2023-05-02 00:51:51 -0300

    fix(req04/05/06/07): update Inventory class

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -4,10 +4,12 @@ from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
+reports = {"simples": SimpleReport, "completo": CompleteReport}
+
 
 class Inventory:
-    @staticmethod
-    def import_data(path):
+    @classmethod
+    def import_data(cls, path, type):
         if path.endswith(".csv"):
             data = CsvImporter.import_data(path)
         elif path.endswith(".json"):
@@ -15,13 +17,4 @@ class Inventory:
         elif path.endswith(".xml"):
             data = XmlImporter.import_data(path)
 
-        return Inventory.generate(type, data)
-
-    @staticmethod
-    def generate(type, data):
-        if type == "simples":
-            report = SimpleReport.generate(data)
-        elif type == "completo":
-            report = CompleteReport.generate(data)
-
-        return report
+        return reports[type].generate(data)
