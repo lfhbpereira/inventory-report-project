6f703faf7c960539a08554920a68a5a319db1d7c
Luiz Pereira <lfhbpereira@gmail.com>
2023-05-02 01:21:06 -0300

    test(req09): add test for report decorator

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,24 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    product = [
+        {
+            "id": "1",
+            "nome_do_produto": "Nicotine Polacrilex",
+            "nome_da_empresa": "Target Corporation",
+            "data_de_fabricacao": "2021-02-18",
+            "data_de_validade": "2023-09-17",
+            "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+            "instrucoes_de_armazenamento": "instrucao 1"
+        }
+    ]
+
+    colored_simple_report = ColoredReport(SimpleReport).generate(product)
+
+    colored_complete_report = ColoredReport(CompleteReport).generate(product)
+
+    assert ("\033[36m" in colored_simple_report) is True
+    assert ("\033[36m" in colored_complete_report) is True
