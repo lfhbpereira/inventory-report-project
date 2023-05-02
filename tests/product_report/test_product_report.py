f2208fdf33bba430205989cd3714fce7ec1e3a3a
Luiz Pereira <lfhbpereira@gmail.com>
2023-05-02 01:08:44 -0300

    test(req08): add test for product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "1234",
+        "ao abrigo de luz"
+    )
+
+    assert (
+        product.__repr__() == "O produto farinha"
+        " fabricado em 01-05-2021"
+        " por Farinini com validade"
+        " até 02-06-2023"
+        " precisa ser armazenado ao abrigo de luz."
+    )
